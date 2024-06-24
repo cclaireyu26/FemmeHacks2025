0e3ceecd8d2412f4fcea0172293c2ddd9c3895b4
rxh2738 <rudhmilahoque@gmail.com>
2024-06-24 13:14:48 -0500

    Initial Commit

---
+++ MindfulMoments/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MindfulMoments
+//
+//  Created by Rudhmila Hoque on 6/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MindfulMoments/MindfulMomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindfulMomentsApp.swift
+//  MindfulMoments
+//
+//  Created by Rudhmila Hoque on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct MindfulMomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

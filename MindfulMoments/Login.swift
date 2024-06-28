e38d0ab3b81daddf8236c1755c5ae4072f84141a
claireyu26 <161135733+claireyu26@users.noreply.github.com>
2024-06-27 22:21:47 -0400

    tried incorporating code to transfer variables to home screen logged in.

--- MindfulMoments/Login.swift
+++ MindfulMoments/Login.swift
@@ -9,10 +9,12 @@ import SwiftUI
 
 struct Login: View {
 
-    @State var username=""
+    //@State var username=""
+    @Binding var enteredUsername: String
     @State var password=""
 
     @State private var navigateToHome=false
+    @State private var hideLoginView=true
 
     var body: some View {
         
@@ -23,7 +25,7 @@ struct Login: View {
                 Text("Login to an existing account.\n").font(.custom("Cochin", fixedSize: 25)).foregroundColor(Color(red: 5 / 255, green: 10 / 255, blue: 48 / 255))
                 
                 Text("Username").font(.custom("Cochin", fixedSize: 20)).foregroundColor(Color(red: 5 / 255, green: 10 / 255, blue: 48 / 255))
-                TextField("user123", text:$username).multilineTextAlignment(.center).font(.title3)
+                TextField("user123", text:$enteredUsername).multilineTextAlignment(.center).font(.title3)
                     .border(Color.black, width:2).textFieldStyle(RoundedBorderTextFieldStyle()).cornerRadius(10.0).overlay(RoundedRectangle(cornerRadius:10.0).stroke(.black,lineWidth:2)).padding()
                 Text("Password").font(.custom("Cochin", fixedSize: 20)).foregroundColor(Color(red: 5 / 255, green: 10 / 255, blue: 48 / 255))
                 TextField("*****", text:$password).multilineTextAlignment(.center).font(.title3)
@@ -40,18 +42,20 @@ struct Login: View {
                 NavigationLink(destination: NavBar()  .navigationBarBackButtonHidden(true)){
                     Text("Login").padding()
                     
+                }.onTapGesture{
+                    hideLoginView.toggle()
                 }.font(.custom("Cochin", fixedSize: 18)).foregroundColor(Color(red: 5 / 255, green: 10 / 255, blue: 48 / 255)).background(Color(red: 254 / 255, green: 234 / 255, blue: 160 / 255)).cornerRadius(15.0).shadow(radius:15)
                   
-            }
+            }.opacity(hideLoginView ? 1 : 0)
             
                 }
-            }
+    }
         }
     
     
 
 
 #Preview {
-    Login()
+    Login(enteredUsername: .constant("test"))
 }
 

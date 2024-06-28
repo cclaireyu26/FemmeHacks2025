b428f813f089460f77df0994d816b114e6e87e9a
claireyu26 <161135733+claireyu26@users.noreply.github.com>
2024-06-27 22:17:03 -0400

    Update NutritionChecker.swift

--- MindfulMoments/NutritionChecker.swift
+++ MindfulMoments/NutritionChecker.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Student on 6/26/24.
 //
-
 //
 //  Nutrition Rater.swift
 //  MindfulMoments
@@ -12,30 +11,34 @@
 //  Created by Willow Miller on 6/26/24.
 //
 import SwiftUI
+import PhotosUI
 struct NutritionChecker: View {
-    @State private var speed = 50.0
-    @State private var isEditing = false
     @State private var foodName = ""
     @State private var addRequest = ""
     @State private var nameofFood = ""
     var foodLibrary = ["spagetti"]
     
+    @State var showPhotoActionSheet=false
+    
+    @State var showPhotoLibrary=false
+    @State var selectedPhoto: PhotosPickerItem?
+    @State var profileImage=Image("splashscreen-bee")
+    
     var body: some View {
-     
         NavigationStack{
             VStack (spacing:20){
                 Text("Mindful Moments")
                     .font(.title)
                     .fontWeight(.bold)
                     .foregroundColor(Color(hue: 0.522, saturation: 0.294, brightness: 0.911))
-                    .padding()
-                    
+                    .background(Color(red: 254 / 255, green: 234 / 255, blue: 160 / 255))
+                
+                
             }
             
                 //Search Bar
                 
                 HStack{
-                    
                     TextField("Search", text: $foodName)
                         .border(Color.black)
                         .padding(.horizontal, 15.0)
@@ -47,7 +50,6 @@ struct NutritionChecker: View {
                     }
                     
                     .padding(.trailing)
-                    
                
                 }
             
@@ -56,7 +58,7 @@ struct NutritionChecker: View {
                     .multilineTextAlignment(.center)
                 
                 Text("Nutrition Rater")
-                    .font(.title2)
+                    .font(.title)
                     .fontWeight(.bold)
                     .foregroundColor(Color(hue: 0.522, saturation: 0.294, brightness: 0.911))
                     .padding(.top)
@@ -64,22 +66,43 @@ struct NutritionChecker: View {
                    
                 HStack(spacing:32){
                    
-                    Image("uploadicon")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width:150)
+//                    Image("uploadicon")
+//                        .resizable()
+//                        .aspectRatio(contentMode: .fit)
+//                        .frame(width:150)
                     
-                    NavigationLink(destination: UploadPhoto()) {
-                        Text("Upload\nPicture\nHere.").fontWeight(.bold)
-                            .tint(.yellow).font(.largeTitle)
-        
+                    Button("Tap to upload"){
+                        showPhotoActionSheet.toggle()
+                           }.buttonStyle(.bordered)
+                        .tint(.pink).padding(30).font(.title).frame(width:200)
+                    profileImage.resizable().frame(width:120,height:120).padding(30).cornerRadius(30).onTapGesture{
+                        showPhotoActionSheet.toggle()
+                    }
+                    .confirmationDialog("Upload a photo", isPresented: $showPhotoActionSheet){
+                        Button{
+                            showPhotoLibrary.toggle()
+                        } label:{
+                            Text("Photo Library")
+                        }
+                    }
+                    .photosPicker(isPresented: $showPhotoLibrary, selection: $selectedPhoto, photoLibrary: .shared())
+                    .onChange(of: selectedPhoto){ newValue in guard let photoItem = selectedPhoto else{
+                        return
+                    }
+                        Task{
+                            if let photoData=try await photoItem.loadTransferable(type: Data.self),
+                               let uiImage = UIImage(data: photoData){
+                                await MainActor.run{
+                                    profileImage = Image(uiImage:uiImage)
+                                }
+                            }
+                        }
                     }
     
                     
                 }
                     
-              
-
+            
             }
            
          
@@ -89,33 +112,18 @@ struct NutritionChecker: View {
                 
                 
             VStack(alignment: .center){
-                Text("Enter food here.")
-                    .font(.headline)
-                    .fontWeight(.bold)
-                    .foregroundColor(.black)
-                
-                TextField("Name of Food", text: $nameofFood)
+                    TextField("Name of Food", text: $nameofFood)
                     .border(Color.black, width:2)
-                    .foregroundColor(.black)
+                    
+                   
+                    .foregroundColor(/*@START_MENU_TOKEN@*/Color(hue: 0.537, saturation: 0.275, brightness: 0.961)/*@END_MENU_TOKEN@*/)
                     .padding()
-
-               
-                Text("Rate: \(nameofFood)")
-                    .font(.headline)
-                    .fontWeight(.bold)
-                    .foregroundColor(.black)
-                Slider(value: $speed,
-                       in: 0...100,
-                       onEditingChanged: { editing in
-                           isEditing = editing
-                       })
+                
                 Link(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=URL@*/URL(string: "https://www.apple.com")!/*@END_MENU_TOKEN@*/) {
                     Text("Recipe & Ingredients")
-                        .padding()
-                        .border(Color(hue: 0.522, saturation: 0.294, brightness: 0.911), width: 3)
-                        .tint(Color.blue)
-                        .cornerRadius(8)
                 }
+                
+                
                     .padding()
                 
                 
@@ -124,7 +132,6 @@ struct NutritionChecker: View {
                 
             
             .padding(.leading)
-            //insert here
             
         }
     }
@@ -132,4 +139,3 @@ struct NutritionChecker: View {
 #Preview {
     NutritionChecker()
 }
-
